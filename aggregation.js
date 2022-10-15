0c6f89efacfa91d59fbd208989810dd44d9d8e30
Tewo24 <115874008+Tewo24@users.noreply.github.com>
2022-10-15 18:41:04 +0800

    aggregation added
    aggregation added

---
+++ aggregation.js
@@ -0,0 +1,283 @@
+EMPLOYEE
+
+
+[
+
+       
+        {
+            
+            "id" : "01234",
+            "name" : "Amber",
+            "age" : 32,
+            "jobRole" : "Regional Manager",
+            "salary" : "10000"
+        },
+
+        
+        {
+            
+            "id" : "01235",
+            "name" : "Heard",
+            "age" : 23,
+            "jobRole" : "Store Manager",
+            "salary" : "55000"
+        },
+
+      
+        {
+            
+            "id" : "01236",
+            "name" : "Johny",
+            "age" : 22,
+            "jobRole" : "Cashier",
+            "salary" : "67500"
+        },
+
+        
+        {
+          
+            "id" : "01237",
+            "name" : "Depp",
+            "age" : 23,
+            "jobRole" : "Senior Cashier",
+            "salary" : "72500"
+        },
+
+        
+        {
+            
+            "id" : "01238",
+            "name" : "Malone",
+            "age" : 24,
+            "jobRole" : "Bagger",
+            "salary" : "35000"
+        }
+],
+
+ 
+
+
+Items
+
+[
+
+              
+        {
+           
+            "id" : "FoodPack0012341",
+            "name" : "Toblerone Bar - 250g",
+            "price" : "200",
+            "quantity" : "15000",
+            "category" : [ 
+                "chocolates", ]
+               
+            
+        },
+
+        
+        {
+          
+            "id" : "FoodPack0012342",
+            "name" : "Cowshead",
+            "price" : "400",
+            "quantity" : "5000",
+            "category" : [ 
+                "Dairy", 
+                
+            ]
+        },
+
+      
+        {
+     
+            "id" : "FoodPack0012343",
+            "name" : "Eggs - 12 Pack",
+            "price" : "180",
+            "quantity" : "3000",
+            "category" : [ 
+                "poultry", 
+                
+            ]
+        },
+
+       
+        {
+          
+            "id" : "FoodPack0012344",
+            "name" : "Sinigang cut Pork",
+            "price" : "280",
+            "quantity" : "1250",
+            "category" : [ 
+                "poultry", 
+                
+            ]
+        },
+
+        
+        {
+           
+            "id" : "FoodPack0012345",
+            "name" : "Potato - 250g",
+            "price" : "80",
+            "quantity" : "9000",
+            "category" : [ 
+                "vegetables", 
+                
+               
+            ]
+        },
+
+       
+        {
+            
+            "id" : "FoodPack0012346",
+            "name" : Orange - 250g",
+            "price" : "70",
+            "quantity" : "10000",
+            "category" : [ 
+                "Fruits", 
+               
+            ]
+        },
+
+        
+        {
+           
+            "id" : "FoodPack0012347",
+            "name" : "Broccoli - 250g",
+            "price" : "150",
+            "quantity" : "12000",
+            "category" : [ 
+                "vegetables", 
+                
+            ]
+        },
+
+        
+        {
+          
+            "id" : "FoodPack0012348",
+            "name" : "Cheese - 500g",
+            "price" : "100",
+            "quantity" : "1500",
+            "category" : [ 
+                "dairy", 
+               
+            ]
+        }
+],
+
+
+
+
+Payment
+
+[
+
+        
+        {
+            
+            "id" : "FP0012341",
+            "grossAmount" : "200.00",
+            "discounts" : "10",
+            "netAmount" : "189.50",
+            "date/time" : "2021-01-01T16:00:00Z"
+        },
+
+       
+        {
+            
+            "id" : "FP0012342",
+            "grossAmount" : "400.00",
+            "discounts" : "0",
+            "netAmount" : "400.00",
+            "date/time" : "2021-01-01T16:15:55Z"
+        },
+
+        
+        {
+           
+            "id" : "FP0012343",
+            "grossAmount" : "180.00",
+            "discounts" : "15.00",
+            "netAmount" : "165.00",
+            "date/time" : "2021-01-01T16:31:08Z"
+        },
+
+        
+        {
+            
+            "id" : "FP0012344",
+            "grossAmount" : "280.00",
+            "discounts" : "0",
+            "netAmount" : "280.00",
+            "date/time" : "2021-01-01T20:25:52Z"
+        },
+
+        
+        {
+           
+            "id" : "FP0012345",
+            "grossAmount" : "80.00",
+            "discounts" : "5.00",
+            "netAmount" : "75.00",
+            "date/time" : "2021-01-01T08:45:12Z"
+        },
+
+        
+        {
+          
+            "id" : "FP0012346",
+            "grossAmount" : "70",
+            "discounts" : "50.00",
+            "netAmount" : "20.00",
+            "date/time" : "2021-01-01T11:02:35Z"
+        }
+],
+
+
+
+
+PROMO
+
+
+[ 
+
+        
+        {
+           
+            "id" : "PROMO50",
+            "name" : "Biscuit Discount",
+            "period" : 10,
+            "dailySales" : "30, 70, 18, 45, 60, 25, 90"
+        },
+
+        
+        {
+           
+            "id" : "PROMO100",
+            "name" : "Milk Discount",
+            "period" : 2,
+            "dailySales" : "220, 300"
+        },
+
+        
+        {
+           
+            "id" : "PROMO150",
+            "name" : "Bread Discount",
+            "period" : 10,
+            "dailySales" : "150, 120"
+        },
+
+        
+        {
+            
+            "id" : "PROMO200",
+            "name" : "Meat Discount",
+            "period" : 10,
+            "dailySales" : "78, 98, 115, 198, 84, 288, 360"
+        }
+]
+
+
